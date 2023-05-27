f26f3d4f69ac649ff02944817feb18e59ade073b
Rishabh Srivastava <117577787+errishabhsrivastava@users.noreply.github.com>
2023-05-27 19:06:43 +0530

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('chatbot','postgres','123456789',{
+const sequelize = new Sequelize('Blogs','postgres','123456789',{
     host: 'localhost',
     dialect: 'postgres'
 });
